3de192b80c296332a94311d0e3c2c83d91f0bac3
roman janko <RomanJanko@seznam.cz>
2019-11-24 17:24:09 +0100

    adding tour definition

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,51 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
 const typeDefs = `
 type Query {
-  info: String!
+  info: String!,
+  tours: [Tour!]!
+}
+
+type Tour {
+  id: ID!,
+  name: String!,
+  description: String,
+  price: Float!,
+  transport: String
 }
 `
 
-// 2
+const tours = [{
+  id: 1,
+  name: 'Bad Kleinkirchheim',
+  description: 'Austria',
+  price: 1000,
+  transport: 'Own'
+}, {
+  id: 2,
+  name: 'Saalbach',
+  description: 'Austria',
+  price: 1100,
+  transport: 'Bus'
+}]
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
+    info: () => `This is the API of a Hackernews Clone`,
+    tours: () => tours
+  },
+  // Tour: {
+  //   id: parent => parent.id,
+  //   name: parent => parent.name,
+  //   description: parent => parent.description,
+  //   price: parent => parent.price,
+  //   transport: parent => parent.transport
+  // }
 }
 
-// 3
 const server = new GraphQLServer({
   typeDefs,
-  resolvers,
+  resolvers
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
