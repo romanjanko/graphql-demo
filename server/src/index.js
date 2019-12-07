dae8385e1107552d83fcedbcb34f063d0d455623
roman janko <RomanJanko@seznam.cz>
2019-12-07 18:08:57 +0100

    moving schema into a separate file and adding comments

--- server/src/index.js
+++ server/src/index.js
@@ -1,39 +1,48 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-  info: String!,
-  tours: [Tour!]!
-}
-
-type Tour {
-  id: ID!,
-  name: String!,
-  description: String,
-  price: Float!,
-  transport: String
-}
-`
-
 const tours = [{
   id: 1,
   name: 'Bad Kleinkirchheim',
   description: 'Austria',
   price: 1000,
-  transport: 'Own'
+  transport: 'Own',
+  comments: []
 }, {
   id: 2,
   name: 'Saalbach',
   description: 'Austria',
   price: 1100,
-  transport: 'Bus'
+  transport: 'Bus',
+  comments: [{
+    id: 1,
+    text: 'What an awesome destination!'
+  }]
 }]
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`,
     tours: () => tours
   },
+  Mutation: {
+    addComment: (parent, args) => {
+      const { tourId, text } = args
+      const tour = tours.find(t => t.id === Number(tourId))
+
+      if (tour) {
+        const { comments } = tour
+        const comment = {
+          id: comments.length + 1,
+          text
+        }
+
+        comments.push(comment)
+
+        return comment
+      } else {
+        throw new Error('Tour not found!')
+      }
+    }
+  }
   // Tour: {
   //   id: parent => parent.id,
   //   name: parent => parent.name,
@@ -44,7 +53,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
 
