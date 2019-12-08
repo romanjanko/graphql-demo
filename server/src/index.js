3d1f1c22b76a06a232dadfb96ac55ff6168b440c
roman janko <RomanJanko@seznam.cz>
2019-12-08 18:15:34 +0100

    adding singup/login mutations.

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const tours = [{
   id: 1,
@@ -18,6 +20,21 @@ const tours = [{
     text: 'What an awesome destination!'
   }]
 }]
+
+const users = []
+
+const APP_SECRET = 'something_that_is_really_secret'
+function getUserId(context) {
+  const authorizationHeader = context.request.get('Authorization')
+
+  if (authorizationHeader) {
+    const token = authorizationHeader.replace('Bearer ', '')
+    const { userId } = jwt.verify(token, APP_SECRET)
+    return userId
+  }
+
+  throw new Error('Not authenticated.')
+}
 
 const resolvers = {
   Query: {
@@ -40,6 +57,49 @@ const resolvers = {
         return comment
       } else {
         throw new Error(`Tour with id ${tourId} not found!`)
+      }
+    },
+    signup: async (parent, args) => {
+      if (users.find(u => u.name === args.name)) {
+        throw new Error('User with same name already exists.')
+      }
+
+      const password = await bcrypt.hash(args.password, 10)
+
+      const user = { 
+        ...args, 
+        id: users.length + 1,
+        password
+      }
+      users.push(user)
+
+      const token = jwt.sign({ userId: user.id }, APP_SECRET)
+    
+      return {
+        token,
+        user
+      }
+    },
+    login: async (parent, args) => {
+      const { name } = args
+
+      const user = users.find(u => u.name === name)
+
+      if (!user) {
+        throw new Error('No such user found.')
+      }
+    
+      const valid = await bcrypt.compare(args.password, user.password)
+
+      if (!valid) {
+        throw new Error('Invalid password.')
+      }
+    
+      const token = jwt.sign({ userId: user.id }, APP_SECRET)
+    
+      return {
+        token,
+        user
       }
     }
   }
